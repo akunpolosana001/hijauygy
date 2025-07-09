53ffb6b5938a23c67a52720f068bbf595764b470
akunapolosana1112 <polosana799@gmail.com>
2025-07-09 23:29:22 +0700

    Update aseli.js

--- aseli.js
+++ aseli.js
@@ -8,29 +8,27 @@ const DATA_PATH = './data.json'
 const AUTHOR_EMAIL = 'tesaccx9@gmail.com'
 const git = simpleGit()
 
-const display = (msg, type = 'info') => {
-  const time = moment().format('HH:mm')
-  const color = {
-    info: chalk.blue,
-    success: chalk.green,
-    error: chalk.red
-  }
-  console.log(`${chalk.gray(`[ ${time} ]`)} ${color[type](msg)}`)
+const timeLog = () => chalk.gray(`[ ${moment().format('HH:mm')} ]`)
+
+const successLog = () => console.log(`${timeLog()} ${chalk.green('Sukses add!!')}`)
+const failLog = (msg) => {
+  console.log(`${timeLog()} ${chalk.red('Gagal add!!')}`)
+  console.log(chalk.red('>>>>>>'), msg)
 }
 
 const randomDate = () => {
-  const startDate = moment('2008-01-01')
-  const endDate = moment()
-  return moment(startDate).add(random.int(0, endDate.diff(startDate, 'days')), 'days').format()
+  const start = moment('2008-01-01')
+  const end = moment()
+  return moment(start).add(random.int(0, end.diff(start, 'seconds')), 'seconds').toISOString()
 }
 
-const commitOnce = async (n) => {
+const commitOnce = async () => {
   try {
     const date = randomDate()
     const data = {
       date: date,
       commit: {
-        message: `Commit #${n} - ${date}`,
+        message: `Commit - ${date}`,
         author: AUTHOR_EMAIL,
         branch: 'main'
       }
@@ -39,23 +37,20 @@ const commitOnce = async (n) => {
     await jsonfile.writeFile(DATA_PATH, data)
     await git.add(DATA_PATH)
     await git.commit(data.commit.message, { '--date': date })
-    display(`[ #${n} ] Commit Berhasil!!`, 'success')
+
+    successLog()
     return true
-  } catch (e) {
-    display(`[ #${n} ] Commit Gagal!!`, 'error')
+  } catch (err) {
+    failLog(err.message)
     return false
   }
 }
 
 const main = async () => {
-  let count = 1
   while (true) {
-    const success = await commitOnce(count)
-    if (success) {
-      await git.push()
-      count++
-    }
+    const ok = await commitOnce()
+    if (ok) await git.push()
   }
 }
 
-main().catch(e => display(`ERROR: ${e}`, 'error'))
+main().catch((e) => failLog(e.message))
